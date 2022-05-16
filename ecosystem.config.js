94056c4368a5d7f75e820d2ad4005bcca5f31574
hrenaud (NovaGaïa) <renaud.heluin@novagaia.com>
2022-05-16 16:21:14 +0200

    swtich back to pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,13 @@
 module.exports = {
   apps: [
     {
-      name: 'webservices-server',
-      script: 'npm',
-      args: 'start',
+      name: "webservices-server",
+      script: "npm",
+      args: "start",
       env: {
-        NODE_ENV: 'production',
+        NODE_ENV: "production",
       },
+      ignore_watch: ["node_modules", "public", "status.txt"],
     },
   ],
-};
+}
