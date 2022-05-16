94f890252945ad7dda9c6923eb94bab714135514
hrenaud (NovaGaïa) <renaud.heluin@novagaia.com>
2022-05-16 11:31:40 +0200

    remove Express from pm2

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd ./gatsby && npm install && npm run build
-cd ../ws-app && npm install &&  pm2 start ecosystem.config.js
+# cd ../ws-app && npm install &&  pm2 start ecosystem.config.js
