411afca56a44aae4a38c50ece37f8ea79320e899
hrenaud (NovaGaïa) <renaud.heluin@novagaia.com>
2022-05-18 09:01:13 +0200

    add sh files

--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-npm install && npm run build
-# cd ../ws-app && npm install &&  pm2 start ecosystem.config.js
---
+++ onRestart.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "restart webservices-server"
+pm2 restart webservices-server
